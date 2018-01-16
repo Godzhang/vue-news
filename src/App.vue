f97f981625606d76163afdc58f3469e585e90641
godzhang <zhangqijilindaxue@126.com>
2018-01-16 23:43:54 +0800

    vue-news

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <v-header @showSide="show"></v-header>
+  </div>
+</template>
+
+<script>
+import VHeader from '@/components/v-header/v-header';
+
+export default {
+  name: 'app',
+  methods: {
+    show(){
+      this.$refs.sidebar.open()
+    }
+  },
+  components: {
+    VHeader
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/v-header/v-header.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="header" ref="header">
+        <div class="model" :class="model">
+            <span class="navigation" @click="showSidebar"></span>
+            <h2>{{title}}</h2>
+        </div>
+    </div>
+</template>
+<script>
+import {mapGetters} from 'vuex';
+
+export default {
+    props: {
+        title: {
+            type: String,
+            default: '今日热闻'
+        }
+    },
+    methods: {
+        showSidebar(){
+            this.$emit('showSide');
+        }
+    },
+    computed: {
+        model(){
+            return this.isNight ? 'night' : 'morning';
+        },
+        ...mapGetters([
+            'isNight'
+        ])
+    }
+}
+</script>
+<style lang="sass">
+
+</style>
